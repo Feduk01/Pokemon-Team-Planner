be8f96182ad9678bba74d7404b05cf844ba57691
Fedor Tarasenko <fedortarasenko2001@gmail.com>
2023-12-15 15:39:56 +0100

    friday work done

--- js/script.js
+++ js/script.js
@@ -22,54 +22,54 @@ reservMenu.addEventListener('click', fromReservViewToStartView)
 const searchBtn = document.querySelector('#searchPokemonBtn')
 const searchInput = document.querySelector('#search-item')
 
-const lowerCaseName = (string) => {
-    return string.toLowerCase()
+ const lowerCaseName = (string) => {
+     return string.toLowerCase()
 }
 
 searchBtn.addEventListener('click', getPokemon)
+let allPokemons = [];
 
-function getPokemon(e) {
-
-    const name = document.querySelector('#searchInput').value
-    const searchContainer = document.querySelector('.search-container')
-    const pokemonName = lowerCaseName(name)
+fetch('https://pokeapi.co/api/v2/pokemon?limit=1000') 
+    .then(response => response.json())
+    .then(data => {
+        allPokemons = data.results;
+    });
+    function getPokemon(e) {
+        e.preventDefault();
+        const searchText = lowerCaseName(document.querySelector('#searchInput').value);
+        const searchContainer = document.querySelector('.search-container');
+        searchContainer.innerHTML = ''; 
     
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    .then((response) => response.json())
-    .then((data) => {
-        const types = data.types.map(typeInfo => typeInfo.type.name).join(', ')
-        const abilities = data.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ')
-        searchContainer.innerHTML = `
-        <div class="search-item">
-                    <img src="${data.sprites.other["official-artwork"].front_default}" 
-                    alt="${data.name}">
-                    <div class="pokemon-info">
-                        <h3 class="pokemon-name">Name: ${data.name}</h3>
-                        <p class="pokemon-type">Type: ${types}</p>
-                        <p class=".pokemon-skills">Abilities: ${abilities}</p>
-
-                    </div>
-                    <div class="buttons-container">
-                        <button class="add-to-team" id="addToTeam" data-pokemon="${data.name}">Add to Team</button>
-                        <button class="add-to-reserv" id="addToReserv" data-pokemon="${data.name}">Add to Reserv</button>
-                    </div>
-                </div>
-    `
-    }).catch((err) => {
-        console.log('Pokemon not found', err)
-    })
-
-    e.preventDefault()
-}
-
+        allPokemons.filter(pokemon => pokemon.name.includes(searchText)).forEach(pokemon => {
+            fetch(pokemon.url)
+                .then(response => response.json())
+                .then(data => {
+                    const types = data.types.map(typeInfo => typeInfo.type.name).join(', '); 
+                    const abilities = data.abilities.map(abilityInfo => abilityInfo.ability.name).join(', ');
+    
+                    searchContainer.innerHTML += `
+                        <div class="search-item">
+                            <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
+                            <div class="pokemon-info">
+                                <h3 class="pokemon-name">Name: ${data.name}</h3>
+                                <p class="pokemon-type">Type: ${types}</p>
+                                <p class="pokemon-skills">Abilities: ${abilities}</p>
+                            </div>
+                            <div class="buttons-container">
+                      <button class="add-to-team" id="addToTeam" data-pokemon="${data.name}">Add to Team</button>
+                      <button class="add-to-reserv" id="addToReserv" data-pokemon="${data.name}">Add to Reserv</button>
+                        </div>
+                        </div>`;
+                });
+        });
+    }
+    
 
 // Adding pokemons to My Team and Reserv
-
-
-
 let myTeam = []
 let myReserv = []
 
+//Eftersom elementen skapas dynamiskt så måste jag utgå från body för att trigga dem
 document.body.addEventListener('click', function(e) {
     if(e.target && e.target.id ==  'addToTeam'){
         addingToTeam(e)
